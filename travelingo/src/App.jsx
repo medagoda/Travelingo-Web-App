5d33aab3a78f0ae9416d4affd041feb15a95b3eb
Nimesha Medagoda <136103215+medagoda@users.noreply.github.com>
2025-07-21 23:31:41 +0530

    repo initialized

---
+++ travelingo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Sidebar } from './components/Sidebar';
+import { MainContent } from './components/MainContent';
+export function App() {
+  return <div className="flex w-full min-h-screen bg-slate-50 p-5">
+      <div className="w-full max-w-7xl mx-auto bg-white rounded-3xl overflow-hidden shadow-lg flex">
+        <Sidebar />
+        <MainContent />
+      </div>
+    </div>;
+}
